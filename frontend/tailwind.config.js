740b227040f37ef15e27eee7ddf6e3ea3da62d36
captaincoro11 <captaincoro444@gmail.com>
2024-06-13 01:10:59 +0530

    Main

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    colors:{
+      hello:"#161212de",
+      orange:"orange"
+    }
+  },
+  plugins: [],
+}
+
